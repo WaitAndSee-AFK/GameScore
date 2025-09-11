afe1a41e78de369d2bff7e2eedf9fecfa32e42ba
WaitAndSee-AFK <b_banni@inbox.ru>
2025-09-11 20:37:44 +0300

    The application logic is configured

--- app/src/main/java/com/example/gamescore/MainActivity.kt
+++ app/src/main/java/com/example/gamescore/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.gamescore
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var score1 = 0
+    private var score2 = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textViewScoreTeam1: TextView = findViewById(R.id.textScoreTeam1)
+        val textViewScoreTeam2: TextView = findViewById(R.id.textViewScoreTeam2)
+
+        textViewScoreTeam1.text = score1.toString()
+        textViewScoreTeam2.text = score2.toString()
+
+        textViewScoreTeam1.setOnClickListener {
+            textViewScoreTeam1.text = (++score1).toString()
+        }
+        textViewScoreTeam2.setOnClickListener {
+            textViewScoreTeam2.text = (++score2).toString()
+        }
+
     }
 }
